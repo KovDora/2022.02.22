6b45589c631f02a93f01bc3e1249d0c12eb9a8b2
KovacsDora <szt2.gp01.20g@szlgbp.hu>
2022-02-22 09:11:29 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace _2022._02._22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string szoveg = File.ReadAllText("input.txt");
+            string mentes = szoveg.Replace(",", "");
+            string mentes2 = mentes.Replace(".", "");
+            string mentes3 = mentes2.Replace("?", "");
+
+            string[] szótömb = mentes3.Split(' ');
+            Dictionary<string, int> szótár = new Dictionary<string, int>();
+            
+            foreach (string szó in szótömb)
+            {
+                if (szótár.ContainsKey(szó))
+                {
+                    szótár[szó] += 1;
+                }
+                else
+                {
+                    szótár[szó] = 1;
+                }
+            }
+            foreach (string kulcs in szótár.Keys)
+            {
+                Console.WriteLine($"{kulcs}:{szótár[kulcs]} db");
+            }
+        }
+    }
+}
